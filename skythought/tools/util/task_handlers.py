c360026528dda1b6b3d561d04ba49546efea9e53
Eric <erictang000@gmail.com>
2025-01-22 01:38:13 +0000

    amc 23

--- skythought/tools/util/task_handlers.py
+++ skythought/tools/util/task_handlers.py
@@ -840,6 +840,22 @@ class ARCChallengeTaskHandler(TaskHandler):
                 return self.invalid_ans
             return matches[-1].strip()
 
+class AMC23TaskHandler(AIMETaskHandler):
+    def __init__(self):
+        self.dataset = "AI-MO/aimo-validation-amc"
+    
+    @staticmethod
+    def generate_prompt(prompt, model):
+        if MODEL_TO_NAME[model] == "Sky-T1-32B-Preview":
+            return prompt + "\nReturn your final response within \\boxed{{}}"
+        else:
+            return "Return your final response within \\boxed{{}}. " + prompt
+    
+    def load_and_filter_dataset(self, start, end, split="train", source=None, filter_difficulty=False, args=None):
+        dataset = load_dataset(self.dataset)
+        train_data = dataset[split].to_pandas()
+        filtered_data = train_data[train_data['url'].str.contains("2023", na=False)]
+        return filtered_data.iloc[start:end] if end > 0 else filtered_data.iloc[start:]
 
 
 TASK_HANDLERS = {
@@ -854,4 +870,5 @@ TASK_HANDLERS = {
     "LiveCodeBench": LiveCodeBenchTaskHandler,
     "GSM8K": GSM8KTaskHandler,
     "ARC-C": ARCChallengeTaskHandler,
+    "AMC23": AMC23TaskHandler,
 }
